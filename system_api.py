5c10ce2589eda5b419ca498cd86e959273f64bb0
melinadwisafitri <melina18dwi@gmail.com>
2021-12-23 09:03:16 +0700

    penambahan word presparation untuk membuang stop_words

--- system_api.py
+++ system_api.py
@@ -1,55 +1,77 @@
 import nltk
+from nltk.corpus import stopwords
 import pickle
 import numpy as np
 import json
 import random
 import urllib.request
 from tensorflow.keras.models import load_model
+import re
 
+# data yang telah tersimpan maka data akan di load untuk digunakan dalam proses prediksi
+model = load_model('chatbot.h5')
 
-model = load_model('model2.h5')
+# dataset yang digunakan untuk training juga di simpan/diambil berdasarkan resource, dan data disimpan dalam format
+# ".json"
 url = 'https://raw.githubusercontent.com/melinadwisafitri/mental-care/machine-learning/dataset/data.json'
 urllib.request.urlretrieve(url, 'data.json')
+
+# dataset akan di load karena data berjenis json, serta data pickle hasil dari processing text di load juga,
+# supaya data daapat digunkan untuk nantinya
 with open('data.json', 'r') as file:
     dataset = json.load(file)
 
-with open('classes.pkl', 'rb') as c:
-    classess = pickle.load(c)
+with open('kelas.pkl', 'rb') as c:
+    tag = pickle.load(c)
 
-with open('words.pkl', 'rb') as w:
-    word = pickle.load(w)
+with open('kata.pkl', 'rb') as w:
+    question = pickle.load(w)
 
-
-def prepare_data(text):
+# sebelum pertanyaan dari user di prediksi maka perlu dilakukan pengolahan text tersebut meliputi :
+# melakukan tokenizer text pertanyaan menggunakan nltk
+# melakukan penghapusan tanda baca dan merubah text menjadi huruf kecil semua
+def prepare_question(text):
+    stop_word = stopwords.words("indonesian")
     text_word = nltk.word_tokenize(text)
-    return text_word
+    text_lower = [t.lower() for t in text_word]
+    text_punctuation = [re.sub(r'[^\w\s]', '', t) for t in text_lower]
+    text_filter = [t for t in text_punctuation if not t.lower() in stop_word]
+    return text_filter
 
 
-def bag_word(text, words, show_detail=True):
-    text_word = prepare_data(text)
-    bag = [0] * len(word)
+# melakukan perubahan data dari text question dengan membagi text perbagian menggunakan nilai 0 dan 1
+# bag nya/panjangnya diambil dari panjang data text dengan nilai 0 didalamnya
+# kemudian data akan di loop dengan ketentuan
+def bag_of_word(text, words):
+    text_word = prepare_question(text)
+    bag = [0] * len(question)
     for s in text_word:
         for i, w in enumerate(words):
             if w == s:
                 bag[i] = 1
-                if show_detail:
-                    print("found bag %s" %w)
     return np.array(bag)
 
-def predict_chat(text, model):
-    predict = bag_word(text, word, show_detail=False)
+# setelah data diubah kemudian akan dilakukan prediksi terhadap pertanyaan yang diberikan user kemudian hasil
+# prediksi akan clasifikasi supaya bisa mendapat rensponse yang sesuai dengan data data akan diurutkan dari 0 untuk
+# mengambil response, lalu hasil dari pedict tersebut akan disamakan dengan classnya yang disimpan kedalam variable list_value
+def text_classification(text, model):
+    predict = bag_of_word(text, question)
     res = model.predict(np.array([predict]))[0]
     values = [[i, r] for i, r in enumerate(res)]
     values.sort(key=lambda x: x[1], reverse=True)
-    return_list = []
+    list_value = []
     for r in values:
-        return_list.append(({"class": classess[r[0]], "probability": str(r[1])}))
-    return return_list
+        list_value.append(({"class": tag[r[0]]}))
+    return list_value
 
 
-def responses(mental_chat, data_json):
-    tag = mental_chat[0]['class']
-    list_data = data_json['mental_healths']
+# setelah pertanyaan berhasil diklasifikasi pertanyaan akan disesuaikan dengan nama tag nya (tag sebagai kelas dari data)
+# jika nama tag pertanyaan sama dengan salah satu tag di data maka akan memilih response secara random sesuai dengan pertanyaan
+# dan apabila data pertanyaan tidak ada maka akan menghasil jawaban "silahkan ajukan pertanyaan lain"
+# selain itu didalam data ada tag khusus yang akan meresponse data '' yang tidak memiliki kata
+def responses(chat, model_data):
+    tag = chat[0]['class']
+    list_data = model_data['mental_healths']
     global val
     for i in list_data:
         if i['tag'] == tag:
@@ -60,8 +82,10 @@ def responses(mental_chat, data_json):
     return val
 
 
+# kemudian hasil dari penyesuaian question dengan tag,
+# sistem akan memberikan response berdasarkan hasil klasifikasi dan datasetnya
+# hasil ini nantinya yang akan digunakan untuk menampilkan response ke web
 def chat_response(message):
-    mental_dataset = predict_chat(message, model)
+    mental_dataset = text_classification(message, model)
     res = responses(mental_dataset, dataset)
     return res
-
