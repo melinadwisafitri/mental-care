5c21b268ee85c996dc6631254c1668bf028e3c70
melinadwisafitri <melina18dwi@gmail.com>
2021-12-23 09:16:46 +0700

    change off setting element, innerhtml

--- static/js/component/rs.js
+++ static/js/component/rs.js
@@ -1,54 +1,23 @@
+import "../data/hospital.js"
+
 class dataRs extends HTMLElement
 {
-    connectedCallback()
+    set rs10(rs)
     {
-        this.render()
+        this._rs10 = rs;
+        this.render();
     }
 
     render()
     {
         this.innerHTML = `
-        <h3>Daftar Lokasi Rumah Sakit di Indonesia</h3>
-        <div class="row">
-            <div class="col-sm-8">
-                <div class="row">
-                    <div class="col card-1">
-                        <img src="./index_files/bot.png" alt="">
-                        <p>rs</p>
-                    </div>
-                    <div class="col card-1">
-                        <img src="./index_files/bot.png" alt="">
-                        <p>rs</p>
-                    </div>
-                    <div class="col card-1">
-                        <img src="./index_files/bot.png" alt="">
-                        <p>rs</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col card-1">
-                        <img src="./index_files/bot.png" alt="">
-                        <p>rs</p>
-                    </div>
-                    <div class="col card-1">
-                        <img src="./index_files/bot.png" alt="">
-                        <p>rs</p>
-                    </div>
-                    <div class="col card-1">
-                        <img src="./index_files/bot.png" alt="">
-                        <p>rs</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus soluta voluptates totam necessitatibus iure ex labore. Maiores sequi eveniet omnis cumque, suscipit, dolores ullam eligendi repellendus architecto voluptas ducimus temporibus?</p>
-            </div>
+        <div class="col">
+         <div class="col card-1">
+            <img src='${this._rs10.image}' alt="${this._rs10.kota}" style="width:100px">
+            <p>${this._rs10.name}</p>
         </div>
-        <div class="row">
-            <a href="http://127.0.0.1:5500/index.html">read more</a>
-        </div>
-        `
+        </div>`
     }
 }
 
-customElements.define("data-rs", dataRs)
+customElements.define("rs-data", dataRs)
