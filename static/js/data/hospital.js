3748b7f9ca51a8031441104f778c7f52005c2eb9
melinadwisafitri <melina18dwi@gmail.com>
2021-12-23 09:12:55 +0700

    dataset nama rs dan lokasi untuk 10 data

---
+++ static/js/data/hospital.js
@@ -0,0 +1,74 @@
+const rs = [
+    {
+        name : "RSUD Cilacap",
+        kota: "Cilacap",
+        image: "https://i0.wp.com/rsud.cilacapkab.go.id/v2/wp-content/uploads/2019/10/7.png",
+        kelas_rs: "B",
+        alamat: "Jl. Jend. Gatot Subroto No. 28 Cilacap"
+    },
+    {
+        name : "RSUD Majenang",
+        lokasi: "cilacap",
+        image: "https://yt3.ggpht.com/a/AATXAJyfSPsvQtv8zV-OvwU1lNjzamwa0tJpdTZdMNI=s900-c-k-c0xffffffff-no-rj-mo",
+        kelas_rs: "C",
+        alamat: "Jl. Dr. Soetomo No. 54 Majenag Cilacap"
+    },
+    {
+        name : "RSI Fatimah",
+        lokasi: "Cilacap",
+        image: "https://cms.sehatq.com/cdn-cgi/image/f=auto/public/img/hospital_thumb/rs-islam-fatimah-cilacap-HCFH00001302.jpg",
+        kelas_rs: "C",
+        alamat: "Jl. IR. H. Juanda No. 20 Cilacap"
+    },
+    {
+        name : "RS Pertamina",
+        lokasi: "Cilacap",
+        image: "https://th.bing.com/th/id/OIP.U1Sfu0FksrRN9Qx9jlY3MAHaHe?pid=ImgDet&rs=1",
+        kelas_rs: "D",
+        alamat: "Jl. MT. Haryono No. 77 PO. BOX 9 Cilacap"
+    },
+    {
+        name : "RS Santa Maria",
+        lokasi: "Cilacap",
+        image: "https://cms.sehatq.com/cdn-cgi/image/f=auto/public/img/hospital_thumb/rs-santa-maria-pemalang-HCFH00000728.jpg",
+        kelas_rs: "D",
+        alamat: "l. Jend. A. Yani No. 38 Cilacap"
+    },
+    {
+        name : "RSU Aprilia",
+        lokasi: "Cilacap",
+        image: "https://goalkes-images.s3-ap-southeast-1.amazonaws.com/hospitals/uHJiQeCm63Vng5G8OXCvzNgF0qUmQwDMrXMbnrhZ.jpg",
+        kelas_rs: "D",
+        alamat: "jl. Gatot Subroto No. 95 A Cilacap "
+    },
+    {
+        name : "RSU Aghisna Medika Kroya",
+        lokasi: "cilacap",
+        image: "https://th.bing.com/th/id/OIP.MFVYZVlEMAEW_YmyShzL_QAAAA?pid=ImgDet&rs=1",
+        kelas_rs: "D",
+        alamat: "Jl. Yos Sudarso No. 1 Kr.mangu Kroya Cilacap "
+    },
+    {
+        name : "RSU Duta Mulya",
+        lokasi: "cilacap",
+        image: "https://1.bp.blogspot.com/-uZcK0JZRcgk/Xw-abSzHmxI/AAAAAAAAAjM/b5tbWl_TDzYRv1ZhCtcmu_AwZEjn1pZwACLcBGAsYHQ/s1600/rs-duta-mulya.jpg",
+        kelas_rs: "D",
+        alamat: "l. Dr.Wahidin No. 66 A Majenang"
+    },
+    {
+        name : "RS Raffa Majenang",
+        lokasi: "Cilacap",
+        image: "https://goalkes-images.s3-ap-southeast-1.amazonaws.com/hospitals/IkBTXazx9pFq6XOcCtKuZc4iPNNtXn0YDZN758wT.jpg",
+        kelas_rs: "D",
+        alamat: "Jl. Bhayangkara No.69, Jenang Selatan, Jenang, Majenang, Ka. Cilacap"
+    },
+    {
+        name : "RSU Afdila",
+        lokasi: "Cilacap",
+        image: "https://th.bing.com/th/id/OIP.BeAu1ouPyQko1N1ReH7fOwAAAA?pid=ImgDet&rs=1",
+        kelas_rs: "D",
+        alamat: "Jl. Soekarno- Hatta 79, Menganti Cilacap"
+    }
+]
+
+export default rs;
