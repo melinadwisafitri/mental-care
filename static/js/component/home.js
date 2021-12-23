026905fdc0b2604b4eb637edd1b6912c339e0705
melinadwisafitri <melina18dwi@gmail.com>
2021-12-23 20:10:10 +0700

    change content

--- static/js/component/home.js
+++ static/js/component/home.js
@@ -10,9 +10,9 @@ class homeMain extends HTMLElement
         this.innerHTML = `
                 <div class="row justify-center">
                     <div class="col-sm-8 header-content-title">
-                        <h4 style="text-align: center; font-family: &#39;Abel&#39;, sans-serif; font-weight: 700; color: white;">______________ world ______________</h4>
-                        <h2>MENTAL HEALTH</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolores enim atque nisi temporibus corrupti animi nesciunt eos quibusdam eius ipsum dolorem velit, hic impedit veritatis nulla, ad, cupiditate modi!</p>
+                        <h4>______________ world ______________</h4>
+                        <h2>KESEHATAN MENTAL</h2>
+                        <p>Mental adalah hal-hal yang berkaitan dengan batin dan watak manusia. Dengan kata lain, kesehatan mental adalah kondisi ketika batin dan watak manusia dalam keadaan normal, tentram, dan tenang, sehingga dapat menjalankan aktivitas dan menikmati kehidupan sehari-hari</p>
                         <button class="btn btn-warning rounded-pill" style="width: 30%;">
                             <span>
                                 START
