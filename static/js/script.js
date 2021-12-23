b4996812ea269357abbef3fe76f67f2c45d41d61
melinadwisafitri <melina18dwi@gmail.com>
2021-12-23 20:08:38 +0700

    change the data  script

--- static/js/script.js
+++ static/js/script.js
@@ -19,73 +19,16 @@ btnChatClose.addEventListener("click", () => {
     document.querySelector("#btn-chat").style.display = "block";
 })
 
-const btn = document.querySelector('#btn-send')
-const chat = document.querySelector('#input-question')
-const chatList = document.querySelector('#chats')
-btn.addEventListener('click', () => {
-   console.log("abc")
-   const xhr = new XMLHttpRequest();
 
-   xhr.onreadystatechange = () =>{
-       if (xhr.readyState == 4){
-           getDataChat()
-       }
-   }
+const li = document.querySelectorAll(".links");
+const sections = document.querySelectorAll("section");
 
-   xhr.open('POST', '/chat', true)
-   xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded;charset=UTF-8')
-   xhr.send('question=' + chat.value)
-//    xhr.send(JSON.stringify({question: chat.value}))
-   console.log(JSON.stringify({questions: chat.value}))
-
-})
-
-const getDataChat = () => {
-   const xhr = new XMLHttpRequest()
-   xhr.onload = function() {
-       const responseJson = JSON.parse(this.responseText);
-       renderChat(responseJson)
-       console.log(responseJson)
-   }
-   xhr.onerror = () => {
-       console.log(error)
-   }
-
-   xhr.open("GET", '/chat')
-   xhr.send()
+function activeNav(){
+    let len = sections.length;
+    while(--len && window.scrollY + 350 < sections[len].offsetTop){}
+    li.forEach(ltx => ltx.classList.remove("active"));
+    li[len].classList.add("active");
 }
+activeNav();
+window.addEventListener("scroll", activeNav)
 
-const renderChat = (msg) => {
-   const chatList = document.querySelector('#chats')
-   const q = document.querySelector('#input-question').value
-   chatList.innerHTML +=  `
-   <div class="chat-body-user row">
-       <div class="col-7">
-           <div class="chat-body-user-text">
-               <p>${q}</p>
-           </div>
-       </div>
-       <div class="col-2">
-           <div class="avatar rounded-circle">
-               <img src="static/assets/bot.png" alt="avatar-bot">
-           </div>
-       </div>
-   </div>`
-   setTimeout(()=>{
-       chatList.innerHTML += `
-       <div id='chat-default' class="chat-body-system row">
-            <div class="col-2">
-                <div class="avatar rounded-circle">
-                    <img src="static/assets/bot.png" alt="avatar-bot">
-                </div>
-            </div>
-            <div class="col-7">
-                <div class="chat-body-system-text">
-                    <p>${msg.answer}</p>
-                </div>
-            </div>
-        </div>
-        `
-   }, 1000);
-   
-}
